dcd7738b7287bfad4192e666add7ccec7014f5c9
anastasiya <nastya050620031n@gmail.com>
2024-11-13 17:48:57 +0700

    added entering lines

--- vernam-cipher/src/main/java/ru/nsu/Main.java
+++ vernam-cipher/src/main/java/ru/nsu/Main.java
@@ -1,7 +1,43 @@
 package ru.nsu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Test.test();
+        encryptEnteredLines();
+    }
+
+    public static void encryptEnteredLines() {
+        System.out.println("-------------------------");
+        KeyGenerator keyGenerator = new KeyGenerator();
+        VernamCipherEncryptor vernamCipherEncryptor = new VernamCipherEncryptor();
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Введите строку (для выхода введите 'exit'):");
+
+        while (true) {
+            System.out.print("> ");
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Выход из программы.");
+                break;
+            }
+
+            String key = keyGenerator.generateKey(input.length());
+            String encryptedText = vernamCipherEncryptor.encrypt(input, key);
+            String decryptedText = vernamCipherEncryptor.encrypt(encryptedText, key);
+            printResult(key, input, encryptedText, decryptedText);
+        }
+    }
+
+    private static void printResult(String key, String text, String encryptedText, String decryptedText) {
+        System.out.println("Ключ: " + key);
+        System.out.println("Текст для шифрования: " + text);
+        System.out.println("Зашифрованный текст: " + encryptedText);
+        System.out.println("Дешифрованный текст: " + decryptedText);
     }
 }
